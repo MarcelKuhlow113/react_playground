ba0f62bd37c5a9b63dff479299c7f07e3dd556a4
mkuhlow <71514801+mkuhlow@users.noreply.github.com>
2026-05-07 11:20:24 +0200

    added pictures and variables

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ class App extends Component {
         return <React.Fragment>
                 <Navbar/>
                 <div className='product-container'>
-                    <Product/>
-                    <Product/>
-                    <Product/>
-                    <Product/>
-                    <Product/>
+                    <Product image="Solarstadt.png" title="Solarstadt" description="Kaufe dir eine Solarstadt"/>
+                    <Product image="Kanal.jpg" title="Kanal" description="Baue einen Kanal"/>
+                    <Product image="Solarthermie.jpg" title="Solarthermieanlage" description="Kaufe dir eine Solarthermieanlage"/>
+                    <Product image="VertikaleFarm.jpg" title="Vertikale Farm" description="Kaufe dir eine Vertikale Farm für dein Projekt"/>
+                    <Product image="Wasserstoff.jpg" title="Wasserstoff" description="Kaufe dir grünen Wasserstoff"/>
                 </div>
         </React.Fragment>;
     }
